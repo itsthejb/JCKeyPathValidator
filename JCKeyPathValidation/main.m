1c64f9d4b0483054370e5be95257ccdccc28486c
Jonathan Crooke <jon.crooke@gmail.com>
2013-11-26 22:46:54 +0000

    Initial Commit

---
+++ JCKeyPathValidation/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  JCKeyPathValidation
+//
+//  Created by Jonathan Crooke on 26/11/2013.
+//  Copyright (c) 2013 Jon Crooke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
